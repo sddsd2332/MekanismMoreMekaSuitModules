91178ed2ff968cec77d6aed0578f9774d475c6c1
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2025-03-07 19:59:27 +0800

    fix Client-side issues

--- src/main/java/moremekasuitmodules/mixin/matteroverdrive/MixinTileEntityGravitationalAnomaly.java
+++ src/main/java/moremekasuitmodules/mixin/matteroverdrive/MixinTileEntityGravitationalAnomaly.java
@@ -7,12 +7,8 @@ import matteroverdrive.tile.MOTileEntity;
 import matteroverdrive.tile.TileEntityGravitationalAnomaly;
 import mekanism.common.MekanismModules;
 import mekanism.common.content.gear.IModuleContainerItem;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
@@ -26,20 +22,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 @Mixin(TileEntityGravitationalAnomaly.class)
 public abstract class MixinTileEntityGravitationalAnomaly extends MOTileEntity implements IScannable, IMOTickable, IGravitationalAnomaly, ITickable {
-
-    /**
-     * @author sddsd2332
-     * @reason 如果玩家带有重力调整模块，则不会被吸入
-     */
-    @Inject(method = "manageClientEntityGravitation", at = @At(value = "INVOKE", target = "Lmatteroverdrive/tile/TileEntityGravitationalAnomaly;getAcceleration(D)D"), remap = false, cancellable = true)
-    public void GravitationalModulation(World world, CallbackInfo ci) {
-        Minecraft minecraft = Minecraft.getMinecraft();
-        EntityPlayer player = minecraft.player;
-        ItemStack chestStack = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
-        if (chestStack.getItem() instanceof IModuleContainerItem item && item.isModuleEnabled(chestStack, MekanismModules.GRAVITATIONAL_MODULATING_UNIT)) {
-            ci.cancel();
-        }
-    }
 
     /**
      * @author sddsd2332
