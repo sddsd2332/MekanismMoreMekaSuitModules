c23c5c18a69d8da9b1c3f158cb4ded8ab77c3cbf
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2025-11-13 18:32:54 +0800

    更新版本
    修复无法合成绝缘单元的问题

--- src/main/java/moremekasuitmodules/common/config/MekaSuitMoreModulesConfig.java
+++ src/main/java/moremekasuitmodules/common/config/MekaSuitMoreModulesConfig.java
@@ -8,16 +8,22 @@ import mekanism.common.config.options.IntOption;
 
 public class MekaSuitMoreModulesConfig extends BaseConfig {
 
-    public BooleanOption mekaSuitShield = new BooleanOption(this, "moremodules", "mekaSuitShield", true, "Enables the default calculation of the full set of shields");
-    public FloatOption mekaSuitShieldCapacity = new FloatOption(this, "moremodules", "mekaSuitShieldCapacity", 1000.0F, "The default set of base shield values after installation", 1.0F, Float.MAX_VALUE);
-    public BooleanOption mekaSuitRecovery = new BooleanOption(this, "moremodules", "mekaSuitRecovery", true, "Enables the default calculation of the full set of Recovery");
-    public FloatOption mekaSuitRecoveryRate = new FloatOption(this, "moremodules", "mekaSuitRecoveryRate", 10.0F, "The recovery rate of the full set of base shields after the default installation", 0.1F, Float.MAX_VALUE);
-    public IntOption mekaSuitShieldRestoresEnergy = new IntOption(this, "moremodules", "mekaSuitShieldRestoresEnergy", 500, "The amount of energy required whenever the shield recovers a little", 0, Integer.MAX_VALUE);
-    public BooleanOption MekAsuitOverloadProtection = new BooleanOption(this, "moremodules", "MekAsuitOverloadProtection", true, "Allows MekAsuit to intercept direct setHealth with Emergency Rescue installed");
-    public final BooleanOption DRrecipes = new BooleanOption(this, "moremodules", "DRrecipes", true, "Enable the default Draconic Evolution recipe").setRequiresGameRestart(true);
-    public final BooleanOption DRAdditionsrecipes = new BooleanOption(this, "moremodules", "DRAdditionsrecipes", true, "Enable the default Draconic Additions recipe").setRequiresGameRestart(true);
-    public final BooleanOption InfiniteInterception = new BooleanOption(this, "moremodules", "InfiniteInterception", false, "Enable Infinite interception and rescue system unit").setRequiresGameRestart(true);
-    public final BooleanOption TCRecipes = new BooleanOption(this, "moremodules", "TCRecipes", true, "Enable the default Thaumcraft Recipe");
-    public final BooleanOption TCAspectRecipes = new BooleanOption(this, "moremodules", "TCAspectRecipes", true, "Let default Thaumcraft recipes add the elements synthesized from both types of elements into the recipe.");
-    public DoubleOption mekaSuitEnergyUsageItemAttack = new DoubleOption(this, "moremodules", "energyUsageItemattack", 200, "Energy cost per tick attacking entity");
+    public BooleanOption mekaSuitShield = new BooleanOption(this, "mekaSuitShield", true, "Enables the default calculation of the full set of shields");
+    public FloatOption mekaSuitShieldCapacity = new FloatOption(this,  "mekaSuitShieldCapacity", 1000.0F, "The default set of base shield values after installation", 1.0F, Float.MAX_VALUE);
+    public BooleanOption mekaSuitRecovery = new BooleanOption(this,  "mekaSuitRecovery", true, "Enables the default calculation of the full set of Recovery");
+    public FloatOption mekaSuitRecoveryRate = new FloatOption(this,  "mekaSuitRecoveryRate", 10.0F, "The recovery rate of the full set of base shields after the default installation", 0.1F, Float.MAX_VALUE);
+    public IntOption mekaSuitShieldRestoresEnergy = new IntOption(this,  "mekaSuitShieldRestoresEnergy", 500, "The amount of energy required whenever the shield recovers a little", 0, Integer.MAX_VALUE);
+    public BooleanOption MekAsuitOverloadProtection = new BooleanOption(this,  "MekAsuitOverloadProtection", true, "Allows MekAsuit to intercept direct setHealth with Emergency Rescue installed");
+    public final BooleanOption DRrecipes = new BooleanOption(this,  "DRrecipes", true, "Enable the default Draconic Evolution recipe").setRequiresGameRestart();
+    public final BooleanOption DRAdditionsrecipes = new BooleanOption(this,  "DRAdditionsrecipes", true, "Enable the default Draconic Additions recipe").setRequiresGameRestart();
+    public final BooleanOption InfiniteInterception = new BooleanOption(this,  "InfiniteInterception", false, "Enable Infinite interception and rescue system unit").setRequiresGameRestart();
+    public final BooleanOption TCRecipes = new BooleanOption(this,  "TCRecipes", true, "Enable the default Thaumcraft Recipe");
+    public final BooleanOption TCAspectRecipes = new BooleanOption(this,  "TCAspectRecipes", true, "Let default Thaumcraft recipes add the elements synthesized from both types of elements into the recipe.");
+    public DoubleOption mekaSuitEnergyUsageItemAttack = new DoubleOption(this, "energyUsageItemattack", 200, "Energy cost per tick attacking entity");
+
+    @Override
+    public String getCategory() {
+        return "moremodules";
+    }
+
 }
